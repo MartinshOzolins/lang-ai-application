1d7a7e47f5508312857d3e320d9c2fd76fa31497
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-04-04 19:14:30 +0100

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+export default function MainHeader() {
+  return (
+    <div className="w-full flex justify-center px-3 sm:px-7 py-2 fixed top-3">
+      <div className="w-full sm:max-w-lg md:max-w-2xl lg:max-w-3xl flex flex-row px-5 py-2 items-center bg-slate-900  text-white rounded-xl h-14 ">
+        <h2 className="instrument-serif-regular-italic pr-7 text-xl">
+          LangAI{" "}
+        </h2>
+        <div className="flex text-xs space-x-2 w-full justify-end">
+          <p>Features</p>
+          <p>FAQ</p>
+          <p>Contact</p>
+        </div>
+      </div>
+    </div>
+  );
+}
