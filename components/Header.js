c8d1e8ab56dbab278a4778204a3abeba2efdadf9
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-04-07 18:28:56 +0100

    update ui for the header, add some boxed fur features section

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,11 @@
 export default function MainHeader() {
   return (
-    <div className="w-full flex justify-center px-3 sm:px-7 py-2 fixed top-3">
-      <div className="w-full sm:max-w-lg md:max-w-2xl lg:max-w-3xl flex flex-row px-5 py-2 items-center bg-slate-900  text-white rounded-xl h-14 ">
-        <h2 className="instrument-serif-regular-italic pr-7 text-xl">
-          LangAI{" "}
-        </h2>
-        <div className="flex text-xs space-x-2 w-full justify-end">
-          <p className="hover:cursor-pointer hover:text-gray-100">Features</p>
-          <p className="hover:cursor-pointer hover:text-gray-100">FAQ</p>
-          <p className="hover:cursor-pointer hover:text-gray-100">Contact</p>
-        </div>
+    <div className="fixed top-5 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] sm:max-w-lg md:max-w-2xl lg:max-w-3xl flex items-center rounded-xl bg-slate-900 text-white h-14 px-5">
+      <h2 className="instrument-serif-regular-italic text-xl">LangAI </h2>
+      <div className="flex text-xs space-x-2 w-full justify-end">
+        <p className="hover:cursor-pointer hover:text-gray-100">Features</p>
+        <p className="hover:cursor-pointer hover:text-gray-100">FAQ</p>
+        <p className="hover:cursor-pointer hover:text-gray-100">Contact</p>
       </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,25 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex flex-col px-3 sm:px-7 bg-gray-100">
-      <h1 className="sm:text-center instrument-serif-regular-italic text-xl  sm:text-3xl md:text-4xl">
+    <div className="w-screen h-screen flex flex-col px-3 sm:px-7">
+      <h1 className="text-center  instrument-serif-regular-italic text-xl sm:text-3xl md:text-4xl">
         Boost Your Language Skills with AI-Powered Tasks
       </h1>
-      <p className="py-10 sm:text-center mx-auto lg:max-w-xl">
+      <p className="py-10 text-center mx-auto lg:max-w-xl">
         From translations to fill-in-the-blanks, create and download language
         tasks in seconds. LangAI is your personalized language learning
         companion, designed to help you practice and improve with ease.
       </p>
-      <button className=" py-2 bg-slate-900 text-white w-52 ml-3 sm:pl-0 sm:mx-auto rounded-xl text-center">
+      <button className="py-2 bg-slate-900 text-white w-52  rounded-xl mx-auto text-center">
         Generate My First Task
       </button>
-      <div className="mt-10 h-10 bg-black"></div>
+      <h2 className="text-center  instrument-serif-regular-italic text-xl sm:text-2xl md:text-3xl pt-20 sm:pt-30 pb-3 sm:pb-5 md:pb-10 text-center">
+        Features
+      </h2>
+      <div className="flex flex-col space-y-3 sm:flex-row  justify-center sm:space-x-3">
+        <div className="w-full h-80 bg-white shadow-lg rounded-md"></div>
+        <div className="w-full h-80 bg-white shadow-lg rounded-md"></div>
+        <div className="w-full h-80 bg-white shadow-lg rounded-md"></div>
+      </div>
     </div>
   );
 }
