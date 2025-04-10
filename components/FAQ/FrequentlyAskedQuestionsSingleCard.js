450076b92af94316ffbddf2ae336be89aaf2223d
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-04-10 18:24:10 +0100

    update toggling for question card

--- components/FAQ/FrequentlyAskedQuestionsSingleCard.js
+++ components/FAQ/FrequentlyAskedQuestionsSingleCard.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 // MUI components
 import AddIcon from "@mui/icons-material/Add";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function FrequentlyAskedQuestionsSingleCard({
   question,
@@ -14,14 +15,22 @@ export default function FrequentlyAskedQuestionsSingleCard({
   const [isAnswerVisible, setIsAnswerVisible] = useState(false);
 
   return (
-    <div className="w-full max-w-3xl bg-stone-200 rounded-xl px-2 py-6 flex flex-col text-sm sm:text-base inter-regular">
+    <div
+      className="w-full max-w-3xl bg-stone-200 rounded-xl px-2 py-6 flex flex-col text-sm sm:text-base inter-regular hover:cursor-pointer"
+      onClick={() => setIsAnswerVisible((prev) => !prev)}
+    >
       <div className="w-full flex justify-between">
         <div className="flex justify-center items-center">
           <p className="pl-1 pr-5">{emoji}</p>
           <p className="align-middle"> {question}</p>
         </div>
 
-        <AddIcon onClick={() => setIsAnswerVisible((prev) => !prev)} />
+        {!isAnswerVisible && (
+          <AddIcon onClick={() => setIsAnswerVisible((prev) => !prev)} />
+        )}
+        {isAnswerVisible && (
+          <CloseIcon onClick={() => setIsAnswerVisible((prev) => !prev)} />
+        )}
       </div>
       {isAnswerVisible && <p className="py-2 text-gray-700 ">{answer}</p>}
     </div>
