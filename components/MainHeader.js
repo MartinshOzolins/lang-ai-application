716c43255d9800c5a0b3eeff7bf1719795008165
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-04-11 17:15:14 +0100

    update conditional rendering for header

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -1,23 +1,34 @@
+"use client";
+
 import Link from "next/link";
 
+// next.js functions
+import { usePathname } from "next/navigation";
+
 export default function MainHeader() {
+  const pathname = usePathname(); // Corrected spelling of pathname
+
   return (
-    <div className="fixed top-5 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] sm:max-w-lg md:max-w-2xl lg:max-w-3xl flex items-center rounded-xl bg-neutral-900 text-white h-14 px-5">
-      <Link href="/" className="instrument-serif-regular-italic text-xl">
-        LangAI{" "}
-      </Link>
-      <div className="flex text-xs space-x-2 w-full justify-end">
-        <a
-          className="hover:cursor-pointer hover:text-gray-100"
-          href="#features"
-        >
-          Features
-        </a>
-        <a className="hover:cursor-pointer hover:text-gray-100" href="#faq">
-          FAQ
-        </a>
-        <p className="hover:cursor-pointer hover:text-gray-100">Contact</p>
-      </div>
-    </div>
+    <>
+      {pathname === "/" && (
+        <div className="fixed top-5 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] sm:max-w-lg md:max-w-2xl lg:max-w-3xl flex items-center rounded-xl bg-neutral-900 text-white h-14 px-5">
+          <Link href="/" className="instrument-serif-regular-italic text-xl">
+            LangAI{" "}
+          </Link>
+          <div className="flex text-xs space-x-2 w-full justify-end">
+            <a
+              className="hover:cursor-pointer hover:text-gray-100"
+              href="#features"
+            >
+              Features
+            </a>
+            <a className="hover:cursor-pointer hover:text-gray-100" href="#faq">
+              FAQ
+            </a>
+            <p className="hover:cursor-pointer hover:text-gray-100">Contact</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
---
+++ src/app/chat/page.js
@@ -0,0 +1,7 @@
+export default function TaskGeneratorPage() {
+  return (
+    <div className="h-screen w-screen">
+      <p>Hello</p>
+    </div>
+  );
+}
