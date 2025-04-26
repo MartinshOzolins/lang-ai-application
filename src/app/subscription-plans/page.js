f810d06077418c2d5dfefb47a04d36a71d52243f
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-04-26 21:27:15 +0100

    update styling for initial subscription plans page

---
+++ src/app/subscription-plans/page.js
@@ -0,0 +1,13 @@
+export default function SubscriptionPlans() {
+  return (
+    <div className="w-full h-full flex flex-col py-7 px-5 dotted items-center">
+      <div className="w-full max-w-4xl flex flex-col items-center bg-white rounded-md shadow-md py-5 px-5">
+        <h2 className="text-3xl text-gray-800">Terms of Service</h2>
+        <p className="text-base sm:text-lg text-gray-600 pt-3 font-bold text-center">
+          Each plan offers something for everyone — the higher the plan, the
+          more you unlock!
+        </p>
+      </div>
+    </div>
+  );
+}
